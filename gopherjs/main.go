36b7dc3e9f7dd79cd83e138f51275165c0d5f8b1
Hebishima <yuzo.hebishima@tokai-grp.co.jp>
2016-01-15 11:05:04 +0900

    Add gopherjs sample

---
+++ gopherjs/main.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/gopherjs/gopherjs/js"
+	"golang.org/x/tools/present"
+	"html/template"
+	"strings"
+)
+
+var gblTmpl *template.Template
+
+func init() {
+	gblTmpl := present.Template()
+	gblTmpl.Funcs(template.FuncMap{"playable": playable})
+
+	_, err := gblTmpl.Parse(ARTICLE_TMPL)
+	if err != nil {
+		fmt.Println("Template#Parse")
+		panic(err)
+	}
+}
+
+func parseArticle(article string) (*present.Doc, error) {
+	r := strings.NewReader(article)
+	return present.Parse(r, "test", 0)
+}
+
+func render(doc *present.Doc) (*bytes.Buffer, error) {
+	w := bytes.NewBuffer([]byte{})
+	err := doc.Render(w, gblTmpl)
+	if err != nil {
+		return nil, err
+	}
+	return w, nil
+}
+
+func main() {
+
+	js.Global.Get("modified").Call("addEventListener", "click", func() {
+		doc, _ := parseArticle(ARTICLE)
+		w, _ := render(doc)
+		println(w.String())
+	})
+}
+
+func playable(c present.Code) bool {
+	return present.PlayEnabled && c.Play && c.Ext == ".go"
+}
+
+const ARTICLE_TMPL = `
+{/* This is the article template. It defines how articles are formatted. */}
+{{define "root"}}
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>{{.Title}}</title>
+    <link type="text/css" rel="stylesheet" href="/article.css">
+    <meta charset='utf-8'>
+  </head>
+
+  <body>
+    <div id="topbar" class="wide">
+      <div class="container">
+        <div id="heading">{{.Title}}
+          {{with .Subtitle}}{{.}}{{end}}
+        </div>
+      </div>
+    </div>
+    <div id="page" class="wide">
+      <div class="container">
+        {{with .Sections}}
+          <div id="toc">
+            {{template "TOC" .}}
+          </div>
+        {{end}}
+
+        {{range .Sections}}
+          {{elem $.Template .}}
+        {{end}}{{/* of Section block */}}
+
+        {{if .Authors}}
+          <h2>Authors</h2>
+          {{range .Authors}}
+            <div class="author">
+              {{range .Elem}}{{elem $.Template .}}{{end}}
+            </div>
+          {{end}}
+        {{end}}
+      </div>
+    </div>
+<!--
+    <script src='/play.js'></script>
+-->
+  </body>
+</html>
+{{end}}
+
+{{define "TOC"}}
+  <ul>
+  {{range .}}
+    <li><a href="#TOC_{{.FormattedNumber}}">{{.Title}}</a></li>
+    {{with .Sections}}{{template "TOC" .}}{{end}}
+  {{end}}
+  </ul>
+{{end}}
+
+{{define "newline"}}
+{{/* No automatic line break. Paragraphs are free-form. */}}
+{{end}}
+
+{/*
+This is the action template.
+It determines how the formatting actions are rendered.
+*/}
+
+{{define "section"}}
+  <h{{len .Number}} id="TOC_{{.FormattedNumber}}">{{.FormattedNumber}} {{.Title}}</h{{len .Number}}>
+  {{range .Elem}}{{elem $.Template .}}{{end}}
+{{end}}
+
+{{define "list"}}
+  <ul>
+  {{range .Bullet}}
+    <li>{{style .}}</li>
+  {{end}}
+  </ul>
+{{end}}
+
+{{define "text"}}
+  {{if .Pre}}
+  <div class="code"><pre>{{range .Lines}}{{.}}{{end}}</pre></div>
+  {{else}}
+  <p>
+    {{range $i, $l := .Lines}}{{if $i}}{{template "newline"}}
+    {{end}}{{style $l}}{{end}}
+  </p>
+  {{end}}
+{{end}}
+
+{{define "code"}}
+  <div class="code{{if playable .}} playground{{end}}" contenteditable="true" spellcheck="false">{{.Text}}</div>
+{{end}}
+
+{{define "image"}}
+<div class="image">
+  <img src="{{.URL}}"{{with .Height}} height="{{.}}"{{end}}{{with .Width}} width="{{.}}"{{end}}>
+</div>
+{{end}}
+
+{{define "iframe"}}
+<iframe src="{{.URL}}"{{with .Height}} height="{{.}}"{{end}}{{with .Width}} width="{{.}}"{{end}}></iframe>
+{{end}}
+
+{{define "link"}}<p class="link"><a href="{{.URL}}" target="_blank">{{style .Label}}</a></p>{{end}}
+
+{{define "html"}}{{.HTML}}{{end}}
+
+{{define "caption"}}<figcaption>{{style .Text}}</figcaption>{{end}}
+`
+
+const ARTICLE = `
+Title
+Sub Title
+2 Jan 2015
+Tags:test
+
+secondarykey
+Programer
+secondarykey@gmail.com
+http://github.com/secondarykey
+@secondarykey
+
+* Page
+
+Description
+
+* More Page
+`
