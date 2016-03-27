68f30f148c0a7f089b0bbb269dac1539c655b160
secondarykey <secondarykey@gmail.com>
2016-03-28 06:22:32 +0900

    Modified text display

--- app/src/datastore.go
+++ app/src/datastore.go
@@ -150,7 +150,7 @@ func createSubTitle(src string) string {
 	dst = strings.Replace(dst, "*", "", -1)
 
 	if len(dst) > 600 {
-		dst = string([]rune(dst)[0:200])
+		dst = string([]rune(dst)[0:200]) + "..."
 	}
 	return dst
 }
